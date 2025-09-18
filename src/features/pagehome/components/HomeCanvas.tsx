a9b43084d45d15fbd1c88975a0082a0f460b1030
alienthebetrayer <gpichkurov@gmail.com>
2025-09-18 15:32:37 +0300

    Update HomeCanvas.tsx

--- src/features/pagehome/components/HomeCanvas.tsx
+++ src/features/pagehome/components/HomeCanvas.tsx
@@ -32,20 +32,24 @@ export const HomeCanvas = ({ fullscreen, figure }: Props) => {
 
     return (
         <Canvas>
-            <ambientLight intensity={0.15} />
-            <directionalLight position={[2, 2, 2]} />
-            <pointLight position={[2, 0, 0]} color='hsla(240, 80%, 50%, 1.00)' intensity={20} distance={300}/>
-            <pointLight position={[0, 0, 2]} color='hsla(0, 80%, 50%, 1.00)' intensity={20} distance={300}/>
-            <pointLight position={[0, 2, 0]} color='hsla(100, 80%, 50%, 1.00)' intensity={1} distance={300}/>
-            <spotLight position={[6, 0, 0]} penumbra={1} angle={Math.PI / 4} intensity={20} color='#00ffee'/>
-
-            <EffectComposer>
-                <Bloom intensity={10} luminanceThreshold={0.0} luminanceSmoothing={1}/>
-            </EffectComposer>
-            <Figure figure={figure}/>
-            { !isMobile && <Particles/> }
-
-            <OrbitControls ref={orbitRef} autoRotate={!fullscreen} enableDamping={true} enableZoom={fullscreen} enablePan={fullscreen}/>
+            {!isMobile && (
+                <>
+                    <ambientLight intensity={0.15} />
+                    <directionalLight position={[2, 2, 2]} />
+                    <pointLight position={[2, 0, 0]} color='hsla(240, 80%, 50%, 1.00)' intensity={20} distance={300}/>
+                    <pointLight position={[0, 0, 2]} color='hsla(0, 80%, 50%, 1.00)' intensity={20} distance={300}/>
+                    <pointLight position={[0, 2, 0]} color='hsla(100, 80%, 50%, 1.00)' intensity={1} distance={300}/>
+                    <spotLight position={[6, 0, 0]} penumbra={1} angle={Math.PI / 4} intensity={20} color='#00ffee'/>
+        
+                    <EffectComposer>
+                        <Bloom intensity={10} luminanceThreshold={0.0} luminanceSmoothing={1}/>
+                    </EffectComposer>
+                    <Figure figure={figure}/>
+                    <Particles/>
+        
+                    <OrbitControls ref={orbitRef} autoRotate={!fullscreen} enableDamping={true} enableZoom={fullscreen} enablePan={fullscreen}/>
+                </>
+            )}
         </Canvas>
     )
 }
