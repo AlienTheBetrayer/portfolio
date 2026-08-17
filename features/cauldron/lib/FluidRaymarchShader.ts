c37504e6c00fa7a35fb642408fdc76f11a40d0ca
alienthebetrayer <gpichkurov@gmail.com>
2026-08-17 18:17:29 +0300

    parallax tweaked

--- features/cauldron/lib/FluidRaymarchShader.ts
+++ features/cauldron/lib/FluidRaymarchShader.ts
@@ -5,15 +5,15 @@ export const FluidRaymarchShader = () =>
 	new ShaderMaterial({
 		uniforms: {
 			uLightPos: { value: new Vector3(0, 2, 0) },
-			uHeightScale: { value: 1.0 },
+			uHeightScale: { value: 0.1 },
 			uShadowSteps: { value: 16.0 },
 			uShadowDensity: { value: 1.5 },
 			uFluidColor: { value: new Color("#d4f1f9") },
 			uLightColor: { value: new Color("#ffaa44") },
 			uPomMinLayers: { value: 32.0 },
-      uPomMaxLayers: { value: 128.0 },
+      uPomMaxLayers: { value: 192.0 },
       uDensityMap: { value: null },
-      uParallaxStrength: { value: 1.0 }
+      uParallaxStrength: { value: 0.4 }
 		},
 		vertexShader: fluidVertexShader,
 		fragmentShader: fluidFragmentShader,
